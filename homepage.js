076c2da84449bb56d936bd27628af567f454657e
saheer-ASM <mohamedshaheer637@gmail.com>
2025-03-05 23:44:28 +0530

    changes on home pages

--- homepage.js
+++
@@ -1,6 +0,0 @@
-function toggleNav() {
-    var nav = document.getElementById('verticalNav');
-    var content = document.getElementById('mainContent');
-    nav.classList.toggle('active');
-    content.classList.toggle('shifted');
-  }
